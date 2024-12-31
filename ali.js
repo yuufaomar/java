12bc3e691b077dd0ee3853611d2e7edf937933bd
yuusufhaaji215 <yuusufhaaji215@gmail.com>
2024-12-31 15:29:22 +0300

    wa simple java

---
+++ ali.js
@@ -0,0 +1,242 @@
+
+// function sayHi (){
+//     document.write ("hello word" , "<br>" )
+// }
+
+// function myage () {
+//     document.write ( 20 )
+// }
+
+
+// function addition () {
+//     let num1 =10
+//     let num2 =90
+//     document.write (num1+num2 )
+// }
+
+
+
+
+// function multi (a,b,c) {
+  
+//     document.write (2*2*2)
+// }
+
+
+// function substract  () {
+
+// let num1 = prompt ("enter niiumber one")
+// let num2 = prompt ("enter niiumber ")
+// document.write (num1*num2)
+
+// }
+
+
+
+// function myname (name,age,address ) {
+//     document.write (name,  age, address, "<br>" )
+// }
+
+
+// myname ("ali ", 72,  " hodan" )
+// myname ("xasn ", 22 , " karan"  )
+// myname ("abdi ", 59 ,   " karan" )
+// myname ("qalid ", 72,  " karan"  )
+
+
+
+
+// function adiition (num1,num2) {
+//     return num1*num2
+// }
+
+// document.write (adiition (6,9))
+
+
+
+    // fat arrow fuction//
+// let ali = () => {
+//     document.write  ("hello qasim")
+// }
+
+// ali ()
+
+
+// function abdd ()  {
+    
+//     document.write (1,)
+// }
+
+// abdd ()
+
+
+
+
+// substract()
+
+// multi ()
+
+// addition()
+// sayHi ()
+
+// function allk () {
+
+//     document.write (27)
+// }
+
+// allk ()
+
+
+// myage()
+
+
+   //array javascript//
+
+// let  fruit = ["apple", "moos","bnana"];
+// // console.log (fruit [1]);
+
+// // adding , removing  array elements//
+// //    push pap
+
+
+// console.log (fruit);
+
+// fruit.push ("new elemnt")
+// console.log (fruit
+
+// )
+
+
+    //teacher//
+    
+     //array varible
+
+// let ages = [20,11, 10, 49, 20, 14, ,18, 19,]
+
+// let below15= ages.filter (function (newAge) {
+//     return newAge >15
+// })
+
+// document.write (below15)
+
+
+   // array string
+// let newname = ["geedi", "geedi", "geedi","ali", "axmed"] 
+
+// let namegedi= newname.filter (function(newname){
+//     return newname == "geedi"
+// })
+
+// document.write (namegedi)
+
+
+  // mao fuction is same as loop
+
+//   let names = ["ali", "abdi" , "xasan"]
+
+//   names.map (function(items){
+//     document.write (items, "<br>")
+//   })
+
+
+   // finding htm element by id
+
+let elementh1 = document.getElementById("title")
+   // finding  by tag name
+let btn = document.getElementsByTagName("button") [0]
+   // finding by class name//
+let para = document.getElementsByClassName ("abdi")[0]
+     
+
+    //best way finding element//
+// let header= document.querySelector("h5")
+
+// let heading = document.querySelector("#hero")
+
+// heading.innerHTML = "waa javascript .. html"
+
+// heading.style.color = "blue"
+// heading.style.backgroundColor = "red"
+// heading.style.fontSize = "40px"
+// heading.style.paddingTop = "30px"
+    
+
+// let paragraph = document.querySelector ("#hello")
+
+
+
+// let boxing =  document.querySelector ("#daal")
+
+
+// boxing.style.padding = "100px"
+// boxing.style.width = "100px"
+// boxing.style.border="5px solid blue"
+// boxing.style.borderRadius="20px"
+// boxing.style.height="240px"
+
+// let box = document.querySelector ("#point")
+// box.style.padding = "100px"
+// box.style.margin = "10px"
+// box.style.width = "100px"
+// box.style.height="240px"
+// box.style.borderRadius="20px"
+// box.style.border="5px solid orange"
+
+
+// console.log (box)
+
+// let boox = document.querySelector ("#main")
+// boox.style.padding = "100px"
+// boox.style.width = "100px"
+// boox.style.border="5px solid red"
+// boox.style.height="240px"
+// boox.style.borderRadius="20px"
+
+
+
+// let boxparent = document.querySelector ("#boxparent")
+// boxparent.style.display="flex"
+// boxparent.style.alignItems="center"
+// boxparent.style.justifyContent="center"
+// boxparent.style.gap="30px"
+
+
+// console.log(boxparent)
+// console.log (boox)
+// console.log (box)
+// console.log (boxing)
+
+// console.log (boxing)
+// console.log (elementh1)
+// console.log(btn)
+// console.log (para)
+// console.log (header)
+
+
+
+
+
+
+
+// javaScript Eventlisterner//
+
+
+let button=document.querySelector (".btn")
+
+button.addEventListener ("click", function() {
+   document.body.style.backgroundColor= "silver"
+})
+
+
+let box = document.querySelector ("#light")
+
+let button1 =document.querySelector ("#btnOn")
+let button2 =document.querySelector ("#btnOff")
+
+button1.addEventListener ("click", function(){
+   box.style.backgroundColor = "orange"
+})
+
+button2.addEventListener ("click", function(){
+   box.style.backgroundColor="transparent"
+})
---
+++ dom.js
@@ -0,0 +1,7 @@
+let parent=document.querySelector ("#parent")
+
+let box1 = document.querySelector ("box1")
+let box2 = document.querySelector ("box1")
+let box3 = document.querySelector ("box1")
+
+box.style.border =("2px solid red")
