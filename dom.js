dc929588f765b0e90be482db5e3829783f605580
yuusufhaaji215 <yuusufhaaji215@gmail.com>
2025-01-02 13:40:31 +0300

    waa asigment

--- dom.js
+++ dom.js
@@ -1,7 +1,118 @@
-let parent=document.querySelector ("#parent")
+// let heading=document.querySelector (("#tiro"))
 
-let box1 = document.querySelector ("box1")
-let box2 = document.querySelector ("box1")
-let box3 = document.querySelector ("box1")
+// let button1 = document.querySelector ("#btnincrement")
+// let button2 = document.querySelector ("#btnreset")
+// let calling = document.querySelector("#call")
 
-box.style.border =("2px solid red")
+
+// let value = 0
+
+// button1.addEventListener("click",function(){
+//     value++
+
+//     heading.innerHTML=value
+
+// if (value==1){
+//     calling.style.backgroundColor ="red"
+// }
+// if (value==2){
+//     calling.style.backgroundColor ="black"
+// }
+// if (value==3){
+//     calling.style.backgroundColor ="blue"
+// }
+// if (value==4){
+//     calling.style.backgroundColor ="silver"
+// }
+
+// else if  (value==5) {
+//     calling.style.backgroundColor ="white"
+// }
+
+
+
+// })
+
+// let value2 =0
+// button2.addEventListener("click",function (){
+//     value2
+//     heading.innerHTML=value2
+// })
+
+
+
+// let bood=document.querySelector("#body")
+// let heading=document.querySelector("#header")
+// let button1=document.querySelector("#btn1")
+// let button2=document.querySelector("#btn2")
+// let book =document.querySelector("#bool")
+
+
+let heading=document.querySelector("#header")
+let button1=document.querySelector("#btn1")
+let button2=document.querySelector("#btn2")
+
+let value=0
+
+button1.addEventListener("click",function(){
+    value++
+    heading.innerHTML=value
+
+if (value==1){
+    document.body.style.backgroundColor="red"
+}
+
+if (value==2){
+    document.body.style.backgroundColor="orange"
+}
+
+ if (value==3){
+    document.body.style.backgroundColor="silver"
+}
+
+else if (value==4){
+    document.body.style.backgroundColor="white"
+}
+    button2.addEventListener("click",function(){
+        value=0
+        heading.innerHTML=value
+    })
+})
+
+
+
+
+// let button2=document.querySelector("#btn2")
+
+// let value  =0
+
+// button1.addEventListener("click",function(){
+//     value++
+//     heading.innerHTML=value
+
+//     if (value==1){
+
+//         document.body.style.backgroundColor="red"
+//      }
+
+//      if (value==2){
+//         document.body.style.backgroundColor="white"
+//      }
+//      if (value==3){
+//         document.body.style.backgroundColor="orange"
+//      }
+//      else if (value==4){
+//         document.body.style.backgroundColor="orange"
+//      }
+
+
+//      button2.addEventListener("click",function(){
+//         value=0
+//         heading.innerHTML=value
+//     })
+// })
+
+
+
+
+
