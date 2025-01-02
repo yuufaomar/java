27a04c62e95955f8d2fdf69b83b7219df8ceec90
yuusufhaaji215 <yuusufhaaji215@gmail.com>
2025-01-02 13:45:26 +0300

    waa assigmenr

--- ali.js
+++ ali.js
@@ -221,22 +221,3 @@ let para = document.getElementsByClassName ("abdi")[0]
 // javaScript Eventlisterner//
 
 
-let button=document.querySelector (".btn")
-
-button.addEventListener ("click", function() {
-   document.body.style.backgroundColor= "silver"
-})
-
-
-let box = document.querySelector ("#light")
-
-let button1 =document.querySelector ("#btnOn")
-let button2 =document.querySelector ("#btnOff")
-
-button1.addEventListener ("click", function(){
-   box.style.backgroundColor = "orange"
-})
-
-button2.addEventListener ("click", function(){
-   box.style.backgroundColor="transparent"
-})
